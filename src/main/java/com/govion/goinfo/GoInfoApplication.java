d0ac03f4f52ffbf1145950770af9a67e0e740d23
mingddlv <1571142492@qq.com>
2022-08-09 14:26:00 +0800

    build
    初始化环境

---
+++ src/main/java/com/govion/goinfo/GoInfoApplication.java
@@ -0,0 +1,13 @@
+package com.govion.goinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/govion/goinfo/GoInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govion.goinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
